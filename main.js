773c131f4c1fb933286859c032a87ff5ba248309
prg-04 <karanjaevans95@gmail.com>
2022-09-12 16:46:52 +0300

    Add mobile-menu function

--- main.js
+++ main.js
@@ -1,28 +1,38 @@
 // global variables
-const hamburgerMenu = document.querySelector('#hum');
-const mobMenu = document.querySelector('.mob-menu');
-const closebtn = document.getElementById('close');
-const portfolio = document.getElementsByClassName('portfolio-link');
+const hamburgerMenu = document.querySelector("#hum");
+const mobMenu = document.querySelector(".mob-menu");
+const closebtn = document.getElementById("close");
+const portfolio = document.getElementsByClassName("portfolio-link");
+const portfolioAncor = document.getElementsByClassName(
+  "material-symbols-outlined"
+);
 
 // mobile menu appears
 function openMobileMenu() {
-  mobMenu.classList.add('active');
+  mobMenu.classList.add("active");
 }
-hamburgerMenu.addEventListener('click', openMobileMenu);
+hamburgerMenu.addEventListener("click", openMobileMenu);
 
 // mobile menu disappears
 function closeMobileMenue() {
-  mobMenu.classList.remove('active');
+  mobMenu.classList.remove("active");
 }
-closebtn.addEventListener('click', closeMobileMenue);
+
+closebtn.addEventListener("click", closeMobileMenue);
 
 // when user click mobile menu options, the mobile menu disappears
 function linkclose() {
-  mobMenu.classList.remove('active');
+  mobMenu.classList.remove("active");
 }
 
 (function closeLink() {
   for (let i = 0; i < portfolio.length; i += 1) {
-    portfolio[i].addEventListener('click', linkclose);
+    portfolio[i].addEventListener("click", linkclose);
   }
-}());
+})();
+
+(function closeLink() {
+  for (let i = 0; i < portfolio.length; i += 1) {
+    portfolioAncor[i].addEventListener("click", linkclose);
+  }
+})();
